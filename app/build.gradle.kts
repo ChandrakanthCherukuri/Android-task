b606393ff1f44d83bc1dd6fc5e62e039e5291ff7
Chandrakanth <cherukurichandrakanth@gmail.com>
2025-10-23 15:45:19 +0530

    Fix build errors: Update compileSdk and targetSdk to version 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.task"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.task"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
